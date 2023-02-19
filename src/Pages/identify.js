38843c51277427d42dcb4e6e443eaffce3da759a
Hsiang Ling <judy900820@gmail.com>
2023-02-20 01:11:23 +0800

    modify css

--- src/Pages/identify.js
+++ src/Pages/identify.js
@@ -161,7 +161,7 @@ export default class identify extends React.Component {
         .getUserMedia({
           audio: false,
           video: {
-            facingMode: "user"
+            facingMode: "environment"
           }
         })
         .then(stream => {
